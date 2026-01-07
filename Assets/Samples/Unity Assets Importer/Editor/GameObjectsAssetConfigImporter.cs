b4bfa6dab4db9a3332a7b189edd02e04599a484a
CoderGamester <game.gamester@gmail.com>
2026-01-07 23:43:25 +0000

    Ui Service and project updated

--- Assets/Samples/Unity Assets Importer/Editor/GameObjectsAssetConfigImporter.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-namespace GameLoversEditor.AssetsImporter
-{
-	public class GameObjectsAssetConfigImporter : AssetsConfigsImporter<GameObjectAssetType, GameObject, GameObjectAssetConfigs> 
-	{ 
-	}
-}
